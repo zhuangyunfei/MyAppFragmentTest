f9c4e471c9291d052186152d35946d0335ea5818
zhuangyunfei <33480828+zhuangyunfei@users.noreply.github.com>
2018-01-19 16:11:48 +0800

    第二次提交
    通过xml文件实现动态添加Fragment

---
+++ app/src/main/java/com/example/myappfragmenttest/FragmentTwo.java
@@ -0,0 +1,24 @@
+package com.example.myappfragmenttest;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * 项目名称
+ * 创建人
+ * 创建时间
+ * 说明
+ */
+
+public class FragmentTwo extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment2,container,false);
+    }
+}
